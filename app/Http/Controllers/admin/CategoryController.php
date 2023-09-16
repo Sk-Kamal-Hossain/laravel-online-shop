11fce9e77d73b125dc0d739d7b0fdbbdf90e1231
Sk-Kamal-Hossain <skkamal6301@gmail.com>
2023-09-16 20:50:02 +0600

    create categories and list categories done

--- app/Http/Controllers/admin/CategoryController.php
+++ app/Http/Controllers/admin/CategoryController.php
@@ -5,11 +5,19 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    public function index(){
-        
+    public function index(Request $request){
+        $categories = Category::latest();
+
+        if(!empty($request->get('keyword'))){
+            $categories = $categories->where('name', 'like', '%'.$request->get('keyword').'%');
+        }
+        $categories = $categories->paginate(10); //orderBy('created_at', 'DESC')
+        //$data['categories'] = $categories;
+        return view('admin.category.list', compact('categories'));
     }
 
     public function create(){
@@ -24,6 +32,18 @@ class CategoryController extends Controller
 
         if ($validator->passes()){
 
+            $category = new Category();
+            $category->name = $request->name;
+            $category->slug = $request->slug;
+            $category->status = $request->status;
+            $category->save();
+
+            $request->session()->get('success', 'Category Added Success!');
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Category Added Success!'
+            ]);
         }
         else{
             return response()->json([
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
         // \App\Models\User::factory(10)->create();
 
@@ -20,5 +20,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\Category::factory(30)->create();
     }
 }
